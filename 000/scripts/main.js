3b9195d4736afec30a7cb5da80f9d74bbc403086
jacklee2020716 <jacklee2020716@gmail.com>
2023-04-03 02:02:54 -0700

    000

---
+++ 000/scripts/main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector("h1");
+
+console.log('Before:', myHeading.textContent); // before change
+
+myHeading.textContent = "Hello world!";
+
+console.log('After:',myHeading.textContent); // after change
+
+
